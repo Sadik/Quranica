0f4d49cbdccbecde7c4e3dafe148f743570d9771
Sadik Özoguz <sadikoezoguz@googlemail.com>
2021-12-28 01:30:05 +0100

    initial commit

---
+++ src/main/java/de/quranica/HelloController.java
@@ -0,0 +1,13 @@
+package de.quranica;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "Hello";
+    }
+}
---
+++ src/test/java/de/quranica/QuranicaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.quranica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuranicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
