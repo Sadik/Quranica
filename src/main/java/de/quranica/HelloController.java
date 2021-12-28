6a1241615bca17671ad811acf2c289f48880fc30
Sadik Özoguz <sadikoezoguz@googlemail.com>
2021-12-28 16:51:20 +0100

    add two basic views

--- src/main/java/de/quranica/HelloController.java
+++ src/main/java/de/quranica/HelloController.java
@@ -1,13 +1,12 @@
 package de.quranica;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     
-    @GetMapping("/")
-    public String index() {
-        return "Hello";
-    }
+//    @GetMapping("/")
+//    public String index() {
+//        return "Hello";
+//    }
 }
